ff5f455278244fc152804b3f07c7a039eda89ff0
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-20 13:03:45 -0500

    Fix Home page indicator inset import

--- app/src/main/java/com/goreecloud/launcher/MainActivity.kt
+++ app/src/main/java/com/goreecloud/launcher/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
